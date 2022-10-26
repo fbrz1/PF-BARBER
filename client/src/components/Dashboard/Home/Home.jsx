c33958f6c00a907bf6c42f5d2697e2d966307b2f
Horo9898 <martinhorodeski@gmail.com>
2022-10-26 00:13:41 -0300

    componente featured

--- client/src/components/Dashboard/Home/Home.jsx
+++ client/src/components/Dashboard/Home/Home.jsx
@@ -3,6 +3,7 @@ import "../Home/Home.scss"
 import NavbarDash from '../Navbar/Navbar'
 import Sidebar from '../Sidebar/Sidebar'
 import Widget from '../Widget/Widget'
+import Featured from '../Featured/Featured'
 
 
 function HomeDash() {
@@ -13,21 +14,18 @@ function HomeDash() {
       <Sidebar />
 
       <div className="homeContainer">
-        container
+        <NavbarDash />
         <div className="widgets">
-          <Widget type = "user"/>
-          <Widget type = "order"/>
-          <Widget type = "earning"/>
-          <Widget type = "balance"/>
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earning" />
+          <Widget type="balance" />
         </div>
-        {/* container aca iria navbar */}
 
-      <div className="homeContainer"> 
-        {/* container */}
-
-        <NavbarDash/>
-       Welcome
-      </div>
+        <div className='charts'>
+          <Featured />
+          <h2 style={{flex: 4}}>Componente barbi</h2>
+        </div>
 
       </div>
     </div>
