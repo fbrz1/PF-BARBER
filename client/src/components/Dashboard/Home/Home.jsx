dffe3608872b1df4489dd86059656d7efc22ef28
brbnoriega <barbaranoriega330@gmail.com>
2022-10-26 00:25:34 -0300

    commit chart

--- client/src/components/Dashboard/Home/Home.jsx
+++ client/src/components/Dashboard/Home/Home.jsx
@@ -4,6 +4,7 @@ import NavbarDash from '../Navbar/Navbar'
 import Sidebar from '../Sidebar/Sidebar'
 import Widget from '../Widget/Widget'
 import Featured from '../Featured/Featured'
+import Chart from '../Chart/Chart'
 
 
 function HomeDash() {
@@ -13,7 +14,6 @@ function HomeDash() {
     <div className='home'>
       <Sidebar />
 
-<<<<<<< HEAD
       <div className="homeContainer">
         <NavbarDash />
         <div className="widgets">
@@ -25,24 +25,8 @@ function HomeDash() {
 
         <div className='charts'>
           <Featured />
-          <h2 style={{flex: 4}}>Componente barbi</h2>
+          <Chart/>
         </div>
-=======
-      <div className="homeContainer">     
-      <NavbarDash/>
-        <div className="widgets"> 
-        
-   
-          <Widget type = "user"/>
-          <Widget type = "order"/>
-          <Widget type = "earning"/>
-          <Widget type = "balance"/>
-        </div>
-
-      <div className="homeContainer"> 
-       
-      </div>
->>>>>>> 49e3a3107a255ce304038d04167759ea7b389cf0
 
       </div>
     </div>
