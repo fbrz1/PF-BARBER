b979f0000300535a0bbdaf411103c8f0cb2eb39d
Horo9898 <martinhorodeski@gmail.com>
2022-10-26 15:23:58 -0300

    char

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,66 @@
-import React from "react";
+import React, { useEffect, useContext } from "react";
 import { Route } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import DetailProduct from "./components/DetailProducts/DetailProducts";
 import Ecommerce from "./components/Ecommerce/Ecommerce";
 import Home from "./components/Home/Home.jsx";
-import { createProducts, getProducts } from "./store/actions";
-import ShoppingCart from "./components/Shopping/ShoppingCart";
 import AboutUs from "./components/AboutUs/AboutUs.jsx";
+import MercadoPago from "./components/MercadoPago/MercadoPago";
+import Register from "./components/Register/Register.jsx";
+import LoginUser from "./components/LoginUser/LoginUser";
+import { CartContext } from "./components/Shopping/ShoppingCart";
+import { getDBUser } from "./redux/actions";
 import Dashboard from "./components/Dashboard/Dashboard.jsx";
-import HomeNavBar from "./components/HomeNavBar/HomeNavBar.jsx";
+
+
+import HomeNavBar from "./components/HomeNavBar/HomeNavBar"
 
 import "./App.css";
+import ItemCart from "./components/FullCart/FullCart";
+
+//import { createProducts, getProducts } from "./store/actions";
+//import ShoppingCart from "./components/Shopping/ShoppingCart";
+//import ProductItem from "./components/Shopping/ProductsItem";
+//import CartItem from "./components/Shopping/CartItem";
+// Top level App component
+//import { ProvideAuth } from "./use-auth.js";
 //holi
 
+
+
+
 function App() {
+  let { userId } = useContext(CartContext)
+  const dispatch = useDispatch()
+  let user = useSelector(state => state.user)
+  useEffect(() => {
+    if (!Object.keys(user).length && userId) {
+      dispatch(getDBUser(userId))
+    }
+  }, [user])
   return (
     <div className="App">
+
+      <Route path="/" render={({ location }) => {
+        return <HomeNavBar user={user} pathname={location.pathname} />
+      }}>
+
+      </Route>
+
       <Route exact path="/">
         <Home />
       </Route>
+      <Route exact path="/dash">
+        <Dashboard />
+      </Route>
+      <Route exact path="/register">
+        <Register />
+      </Route>
+
+      <Route exact path="/login">
+        <LoginUser />
+      </Route>
+
       <Route exact path="/shop">
         <Ecommerce />
       </Route>
@@ -25,25 +68,20 @@ function App() {
         <AboutUs />
       </Route>
 
+      <Route exact path="/payments/pay">
+        <MercadoPago />
+      </Route>
+
+      <Route exact path="/cart">
+        <ItemCart />
+      </Route>
+
       <Route
-        exact
-        path="/product/:id"
+        exact path="/product/:id"
         render={({ match }) => {
           return <DetailProduct match={match} />;
         }}
-      ></Route>
-
-      <Route exact path="/dash">
-        <Dashboard />
-      </Route>
-
-      {/* <Route path="/" render={({ location }) => {
-        return <HomeNavBar user={user} pathname={location.pathname} />
-      }}> </Route> */}
-
-      {/* <Route path = "/">
-      <HomeNavBar />
-      </Route> */}
+      />
     </div>
   );
 }
