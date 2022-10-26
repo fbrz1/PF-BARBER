261b352bf971e0e2f0a9a16520a071a484993cff
Horo9898 <martinhorodeski@gmail.com>
2022-10-26 00:26:39 -0300

    finish

--- client/src/components/Dashboard/Chart/Chart.jsx
+++ client/src/components/Dashboard/Chart/Chart.jsx
@@ -1,4 +1,4 @@
-import './chart.scss';
+import './Chart.scss';
 import React, { PureComponent } from 'react';
 import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 const data = [
