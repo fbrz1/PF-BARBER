86ed5639713e44926f1f40562d1b5bf35d52ddb4
brbnoriega <barbaranoriega330@gmail.com>
2022-10-28 18:58:45 -0300

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -70,7 +70,7 @@ function App() {
       <Route exact path="/dash/users/:id">
         <Single />
       </Route>
-      <Route exact path="/dash/products/:id">
+      <Route exact path="/dash/product/:id">
         <SingleProduct />
       </Route>
 
